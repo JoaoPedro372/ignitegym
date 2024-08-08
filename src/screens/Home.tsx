90be1d07b11024a7cf7e838d14680abea767bbfd
João Pedro <jp.dovale@hotmail.com>
2024-08-08 17:23:26 -0300

    Inserindo navegação com buttonTabNavigator

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { HomeHeader } from "@components/HomeHeader";
+import { Center, VStack } from "@gluestack-ui/themed";
+
+
+export function Home(){
+  return(
+   <VStack flex={1}>
+    <HomeHeader />
+   </VStack>
+  )
+}
